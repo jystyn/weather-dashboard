1ac17a74b1dd79d1075b22ec94092b1f03c0fc35
joostyn <justyn.helgeson@gmail.com>
2022-07-11 20:39:18 -0500

    Found how to get the 5th day to populate

--- Assets/script.js
+++ Assets/script.js
@@ -7,7 +7,7 @@ var state;
 var listCityNames = JSON.parse(localStorage.getItem('City')) || [];
 //Call a function that populates search history under search bar
 
-
+//The start up page function that populates the search history if there is information in local storage
 function start() {
     for (var i = 0; i < listCityNames.length; i++) {
         var $liEl = $('<li>').text(`${listCityNames[i].city}, ${listCityNames[i].state}`);
@@ -20,6 +20,7 @@ function start() {
     $('#search-history li').on("click", previousLocation);
 }
 
+//This finds the coordinates for the city selected from search history
 function previousLocation() {
     var storedCityEl = this;
     var storedCity = storedCityEl.innerText;
@@ -39,6 +40,25 @@ function previousLocation() {
         .catch(console.err);
 }
 
+//This finds the weather from the search history
+function fetchPreviousWeather(data) {
+    var lat = data[0].lat;
+    var lon = data[0].lon;
+    var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`;
+    city = data[0].name;
+    console.log(state);
+
+    fetch(url)
+        .then((resp) => {
+            if (!resp.ok) throw new Error(resp.statusText);
+            return resp.json();
+        })
+        .then((data) => {
+            currentWeather(data);
+            fiveDayForecast(data);
+        })
+        .catch(console.err);
+}
 
 //This function finds the coordinates of of the selected city and passes them to the next function
 function fetchLocation(event) {
@@ -46,7 +66,7 @@ function fetchLocation(event) {
     city = $('#search-city-name').val();
     state = $('#select-state').val();
     var url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&limit=5&appid=${key}`;
- 
+
     fetch(url)
         .then((resp) => {
             if (!resp.ok) throw new Error(resp.statusText);
@@ -66,33 +86,9 @@ function fetchWeather(data) {
     city = data[0].name;
     var storeCity = { city, state };
 
-    // This stores city names into Local Storage
-    // How can we make it show the search history doesn't show duplicates?
-    if (!listCityNames.includes(storeCity)) {
-        listCityNames.push(storeCity);
-        localStorage.setItem("City", JSON.stringify(listCityNames));
-    } else {
-        console.log('duplicate!');
-    }
-
-    fetch(url)
-        .then((resp) => {
-            if (!resp.ok) throw new Error(resp.statusText);
-            return resp.json();
-        })
-        .then((data) => {
-            currentWeather(data);
-            fiveDayForecast(data);
-        })
-        .catch(console.err);
-}
-
-function fetchPreviousWeather(data) {
-    var lat = data[0].lat;
-    var lon = data[0].lon;
-    var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`;
-    city = data[0].name;
-    console.log(state);
+    // This stores city names into local storage
+    listCityNames.push(storeCity);
+    localStorage.setItem("City", JSON.stringify(listCityNames));
 
     fetch(url)
         .then((resp) => {
@@ -132,12 +128,16 @@ function currentWeather(data) {
     }
 }
 
+//This function give the dates for the next 5 days as well as the weather info
 function fiveDayForecast(data) {
     $('#day-1-date').text(moment().add(1, 'days').format("MM/DD/YY"));
     $('#day-2-date').text(moment().add(2, 'days').format("MM/DD/YY"));
     $('#day-3-date').text(moment().add(3, 'days').format("MM/DD/YY"));
     $('#day-4-date').text(moment().add(4, 'days').format("MM/DD/YY"));
     $('#day-5-date').text(moment().add(5, 'days').format("MM/DD/YY"));
+
+    // I couldn't figure out why my .each wasn't populating all 5 days, so I decided to add a 6th day and make it go hidden so the 5th day is populated
+    $('#day-6').addClass("hidden");
 
     //This .each method allows us to repeat processes instead of repeating the same code
     $(`.daily-weather-card`).each(function (i) {
