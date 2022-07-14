f6556b4256b32d812cd1dd3506b4470e25bb303c
joostyn <justyn.helgeson@gmail.com>
2022-07-13 21:29:57 -0500

    Fixed search history missing state name issue

--- Assets/script.js
+++ Assets/script.js
@@ -2,8 +2,7 @@
 var key = '66108857e5269e30957c9122ce9d9d84';
 
 //these variables will help us make our search history
-var city;
-var state;
+var city, state;
 var listCityNames = JSON.parse(localStorage.getItem('City')) || [];
 //Call a function that populates search history under search bar
 
@@ -24,9 +23,9 @@ function start() {
 function previousLocation() {
     var storedCityEl = this;
     var storedCity = storedCityEl.innerText;
-    var city = storedCity.slice(0, -4);
-    var state = storedCity.slice(-2);
-    var url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&limit=5&appid=${key}`;
+    city = storedCity.slice(0, -4);
+    state = storedCity.slice(-2);
+var url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&limit=5&appid=${key}`;
 
     fetch(url)
         .then((resp) => {
@@ -34,8 +33,8 @@ function previousLocation() {
             return resp.json();
         })
         .then((data) => {
-            fetchPreviousWeather(data);
-        })
+            fetchPreviousWeather(data);      
+;        })
         .catch(console.err);
 }
 
@@ -57,7 +56,6 @@ function fetchPreviousWeather(data) {
         })
         .catch(console.err);
 }
-
 //This function finds the coordinates of of the selected city and passes them to the next function
 function fetchLocation(event) {
     event.preventDefault();
@@ -84,9 +82,9 @@ function fetchWeather(data) {
     city = data[0].name;
     var storeCity = { city, state };
 
-    // This stores city names into local storage
-    listCityNames.push(storeCity);
-    localStorage.setItem("City", JSON.stringify(listCityNames));
+        listCityNames.push(storeCity);
+        var cityNames = JSON.stringify(listCityNames);
+        localStorage.setItem("City", cityNames);
 
     fetch(url)
         .then((resp) => {
@@ -104,18 +102,27 @@ function fetchWeather(data) {
 function currentWeather(data) {
     var today = moment().format("MM/DD/YY");
     var icon = data.current.weather[0].icon;
+    var iconAlt = data.current.weather[0].description;
     var temp = data.current.temp;
     var wind = data.current.wind_speed;
     var humidity = data.current.humidity;
     var uvIndex = data.current.uvi;
 
+    var $divEl = $('<div>').html(
+        `<section id="current-weather">
+            <h2>${city}, ${state} (${today})</h2>
+            <img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="${iconAlt}">
+            <li>Temp: ${temp}℉</li>
+            <li>Wind: ${wind} mph</li>
+            <li>Humidty: ${humidity}%</li>
+            <li>UV Index: <span id="uv-color">${uvIndex}</span></li>
+        </section>`
+    ) 
+    
+    $('#weather-container').prepend($divEl);
+    
     $('#weather-container').removeClass("hidden");
-    $('#current-city-name').text(`${city}, ${state} (${today})`);
-    $('#current-icon').attr("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
-    $('#current-temp').text(`Temp: ${temp}℉`);
-    $('#current-wind').text(`Wind: ${wind} mph`);
-    $('#current-humidity').text(`Humidity: ${humidity}%`);
-    $('#uv-index').html(`<li>UV Index: <span id="uv-color">${uvIndex}</span></li>`);
+    
 
     if (uvIndex <= 2) {
         $('#uv-color').css("background-color", "green");
@@ -128,24 +135,25 @@ function currentWeather(data) {
 
 //This function give the dates for the next 5 days as well as the weather info
 function fiveDayForecast(data) {
-    $('#day-0-date').text(moment().add(1, 'days').format("MM/DD/YY"));
-    $('#day-1-date').text(moment().add(2, 'days').format("MM/DD/YY"));
-    $('#day-2-date').text(moment().add(3, 'days').format("MM/DD/YY"));
-    $('#day-3-date').text(moment().add(4, 'days').format("MM/DD/YY"));
-    $('#day-4-date').text(moment().add(5, 'days').format("MM/DD/YY"));
-
-    //This .each method allows us to repeat processes instead of repeating the same code
-    $(`.daily-weather-card`).each(function (i) {
+    for (var i = 0; i < 5; i++) {
+        var date = moment().add(1 + i, 'days').format("MM/DD/YY");
         var icon = data.daily[i].weather[0].icon;
+        var iconAlt = data.daily[i].weather[0].description;
         var temp = data.daily[i].temp.day;
         var wind = data.daily[i].wind_speed;
         var humidity = data.daily[i].humidity;
-       
-        $(`#day-${i}-icon`).attr("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
-        $(`#day-${i}-temp`).text(`Temp: ${temp}℉`);
-        $(`#day-${i}-wind`).text(`Wind: ${wind} mph`);
-        $(`#day-${i}-humidity`).text(`Humidity: ${humidity}%`);
-    })
+        
+        var $divEl = $('<div>').html(`
+        <div class="daily-weather-card" id="day-${i}">
+            <h4>${date}</h4>
+            <img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="${iconAlt}">
+            <li>Temp: ${temp}℉</li>
+            <li>Wind: ${wind} mph</li>
+            <li>Humidty: ${humidity}%</li>
+        </div>`);
+
+        $('#days-container').append($divEl);
+    }
 }
 
 start();
